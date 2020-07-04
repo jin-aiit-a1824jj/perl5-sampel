d08993b7148fd0fcfd4b4dd9934a5d9ad765562a
JIN <a1824jj@aiit.ac.jp>
2020-07-04 08:54:12 +0000

    groups:Finding out what you actually matched

---
+++ session1/tutorial8-main.pl
@@ -0,0 +1,20 @@
+use strict;
+use warnings;
+
+$|=1;
+
+sub main {
+ my $file = '/home/pbl/udemy-perl5-tutorial/session1/mymanjeeves.txt';
+  
+  open(INPUT, $file) or die "Input file $file not found.";
+
+  while(my $line = <INPUT>) {
+    if($line =~ /(I..a.)(...)/){
+      print "First match:'$1'; second match:'$2'\n";
+    }
+  }
+
+  close(INPUT);
+}
+
+main();
