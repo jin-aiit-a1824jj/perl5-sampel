5c9f23f9e4b3316fe0a212a5c740a70a3fd08f7f
JIN <a1824jj@aiit.ac.jp>
2020-07-05 07:58:17 +0000

    character classes

---
+++ session3/tutorial26-main.pl
@@ -0,0 +1,25 @@
+use strict;
+use warnings;
+
+$|=1;
+
+sub main {
+
+  #Ranges
+  #[0-9] any number
+  #[A-Z] any uppercase letter (in the English alphabet)
+  #[ABC] -- list of alternates
+  #[A-Za-z_0-9] -- specify alternatives just by listing then.
+  #[\=\%] - somply list alternatives. Backslash any character that might have a special meaning in regex
+
+  my $content = "The 39 Steps - a GREAT book - Colurs_15 ==%== ABBCCBBCCABCA";
+
+  if($content =~ /([^0-9T\s]{5,})/) {
+    print "Matched '$1'\n";
+  }else {
+    print "No match\n";
+  }
+
+}
+
+main();
