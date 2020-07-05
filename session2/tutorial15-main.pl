8e6f620e1d553b145bdf1e110c5e674a3eb47d39
JIN <a1824jj@aiit.ac.jp>
2020-07-05 04:41:22 +0000

    Chomp & Removing Spaces in splits

---
+++ session2/tutorial15-main.pl
@@ -0,0 +1,29 @@
+use strict;
+use warnings;
+
+use Data::Dumper;
+
+$|=1;
+
+sub main {
+  my $input ='test.csv';
+
+  unless(open(INPUT, $input)){
+    die "\nCannot open $input\n";
+  }
+
+  while(my $line = <INPUT>){
+
+    chomp $line;
+    # print "'$line'\n";
+
+    my @values = split /\s,*\s*/, $line;
+
+    # print join '|', @values;
+    print Dumper(@values);
+  }
+
+  close INPUT;
+}
+
+main();
