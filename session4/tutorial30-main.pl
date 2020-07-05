00dc9b920aa3b736a05237580f729a8930f5646d
JIN <a1824jj@aiit.ac.jp>
2020-07-05 11:39:27 +0000

    subroutines and returning values

---
+++ session4/tutorial30-main.pl
@@ -0,0 +1,25 @@
+use strict;
+use warnings;
+use Data::Dumper;
+use Getopt::Std;
+
+$|=1;
+
+sub main {
+  my %opts;
+  getopts('af:c', \%opts);
+
+  if(!checkusage()){
+    usage();
+  }
+}
+
+sub checkusage {
+  return 0;
+}
+
+sub usage {
+  print "incorrect options\n";
+}
+
+main();
