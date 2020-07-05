4b2c15326468e6e4287d2f8e14d5ce71ee7acafb
JIN <a1824jj@aiit.ac.jp>
2020-07-05 05:52:57 +0000

    cleaning csv

---
+++ session2/tutorial23-main.pl
@@ -0,0 +1,64 @@
+use strict;
+use warnings;
+
+use Data::Dumper;
+
+$|=1;
+
+sub main {
+  my $input ='test3.csv';
+
+  unless(open(INPUT, $input)){
+    die "\nCannot open $input\n";
+  }
+
+  my @data;
+
+  LINE: while(my $line = <INPUT>){
+
+    $line =~ s/^\s*|\s*$//g;
+    #$line =~ s/\s*$//;
+    $line =~ s/\?|approx\.|\$//g;
+    $line =~ /\S+/ or next;
+    print $line . "\n";
+
+    chomp $line;
+    
+    my @values = split /\s*,\s*/, $line;
+    
+    if(scalar(@values) < 3) {
+      print "Invalid line: $line\n";
+      next LINE;
+    }
+
+    foreach my $value(@values) {
+      if($value eq '') {
+        print "Invalid line: $line\n";
+        next LINE;
+      }
+    }
+    
+    my ($name, $payment, $date) = @values;
+    
+    my %values = (
+      "Name" => $name,
+      "Payment" => $payment,
+      "Date" => $date,
+    );
+
+    push @data, \%values;
+  }
+
+  close INPUT;
+
+  #print Dumper(@data);
+
+  foreach my $data(@data) {
+    print $data->{"Payment"} . "\n";
+  }
+
+  # print "Descartes: ". $data[3]{"Name"} . "; Date: '" . $data[3]{"Date"}."'" . "\n";
+  print "Descartes: $data[3]{'Name'}; Date: '$data[3]{'Date'}'\n";
+}
+
+main();
